a97212d9507faa4da7ebf2c9a3529745ffea3b3c
Anjanette Young <younga3@uw.edu>
2014-12-18 16:36:36 -0800

    Initial commit

---
+++ Rakefile
@@ -0,0 +1,9 @@
+# Add your own tasks in files placed in lib/tasks ending in .rake,
+# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
+
+require File.expand_path('../config/application', __FILE__)
+
+require 'jettywrapper'
+Jettywrapper.hydra_jetty_version = "v8.1.1"
+
+Rails.application.load_tasks
---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = 'b2603e9926fecd1ca49a58e0bc0231b3742db8e539e0771fd43b65851eae64ef50f49c40031841feb7c46020189d77d7010828de70bdd3ecbf7565a1458623bd'
+
