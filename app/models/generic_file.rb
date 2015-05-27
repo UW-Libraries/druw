e2afab4e0ca78aebc85cdc366a1ece9f9e220582
Anjanette <younga3@uw.edu>
2015-05-27 10:33:20 -0700

    Edited generic_file.rb so the predicate reflects the Policy column in the data dictionary.

--- app/models/generic_file.rb
+++ app/models/generic_file.rb
@@ -1,8 +1,8 @@
 class GenericFile < ActiveFedora::Base
   include Sufia::GenericFile
 
-  property :accrual_periodicity, predicate: ::RDF::DC.Frequency
-  property :accrual_policy, predicate: ::RDF::DC.Policy
+  property :accrual_periodicity, predicate: ::RDF::DC.accrualPeriodicity
+  property :accrual_policy, predicate: ::RDF::DC.accrualPolicy
   property :alternative, predicate: ::RDF::DC.alternative
   property :license, predicate: ::RDF::DC.license, multiple: false
 end
