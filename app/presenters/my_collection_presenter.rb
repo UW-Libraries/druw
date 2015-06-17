986f624e635390251b6df1755a1eec9076b630a3
Mahria <mahria@uw.edu>
2015-06-17 09:35:06 -0700

    Made my collection presenter spec less ugly, more effecient. fixed failing terms in collection presenter.

--- app/presenters/my_collection_presenter.rb
+++ app/presenters/my_collection_presenter.rb
@@ -1,7 +1,7 @@
 # app/presenters/my_collection_presenter.rb
 class MyCollectionPresenter < Sufia::CollectionPresenter
   self.model_class = ::Collection
-  self.terms = [:title, :total_items, :size, :resource_type, :description, :creator, :contributor,
+  self.terms = [:title, :total_items, :size, :resource_type, :description, :complex_creators, :contributor,
                 :tag, :rights, :publisher, :date_created, :subject, :language, :grant_award_number, :identifier,
                 :based_near, :related_url, :accrual_periodicity, :accrual_policy, :license]
 end
--- spec/presenters/my_collection_presenter_spec.rb
+++ spec/presenters/my_collection_presenter_spec.rb
@@ -1,14 +1,9 @@
 describe MyCollectionPresenter do
-  it 'includes alternative in its terms' do
-    expect(described_class.terms).to include(:accrual_policy)
-  end
-  it 'includes license in its terms' do
-    expect(described_class.terms).to include(:license)
-  end
-  it 'includes complex creators in its terms' do
-    expect(described_class.terms).to include(:accrual_periodicity)
-  end
-  it 'includes complex creators in its terms' do
-    expect(described_class.terms).to include(:grant_award_number)
+  describe 'terms' do
+   subject { described_class.terms }
+	it { is_expected.to include(:accrual_policy) }
+	it { is_expected.to include(:license) }
+	it { is_expected.to include(:accrual_periodicity) }
+	it { is_expected.to include(:grant_award_number) }	
   end
 end
