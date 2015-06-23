4ab6d29ad55c39ddcd66402ca046eec45a92bf88
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2015-06-23 16:18:55 -0700

    Collection form should use class from DRUW codebase. Fixes issue #30.

--- app/forms/my_collection_edit_form.rb
+++ app/forms/my_collection_edit_form.rb
@@ -1,3 +1,12 @@
 class MyCollectionEditForm < MyCollectionPresenter
+  def terms
+    super - [:size, :total_items]
+  end
 
+  # Test to see if the given field is required
+  # @param [Symbol] key a field
+  # @return [Boolean] is it required or not
+  def required?(key)
+    model_class.validators_on(key).any?{|v| v.kind_of? ActiveModel::Validations::PresenceValidator}
+  end
 end
