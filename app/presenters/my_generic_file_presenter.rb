a289952e84865106eb802116384f33ca5e152dea
Mahria <mahria@uw.edu>
2015-07-08 15:44:08 -0700

    Metadata: changed order of some fields, changed label for desc, changed index type for abstract and other date

--- app/presenters/my_generic_file_presenter.rb
+++ app/presenters/my_generic_file_presenter.rb
@@ -1,6 +1,6 @@
 class MyGenericFilePresenter < Sufia::GenericFilePresenter
-  self.terms = [:resource_type, :title, :complex_creators, :contributor, :description, :abstract,
-    :tag, :rights, :license, :publisher, :date_created, :subject, :language,
+  self.terms = [:resource_type, :title, :alternative, :complex_creators, :contributor, :description, :abstract,
+    :tag, :subject, :rights, :license, :publisher, :date_created, :other_date, :temporal, :language,
     :grant_award_number, :identifier, :based_near, :dec_latitude, :dec_longitude, 
-    :related_url, :alternative, :other_date, :temporal, :toc ]
+    :related_url, :toc ]
 end
