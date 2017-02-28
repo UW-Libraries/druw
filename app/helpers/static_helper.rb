2acd19f64683bbb91debe5db7559c3dae0f24842
Kate Deibel <deibel@uw.edu>
2017-02-28 21:06:56 +0000

    Added FAQ page and fixed static title generation

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,10 +1,20 @@
 module StaticHelper
-   def static_page_title
-       default_page_title.gsub('Static','')
+   def get_repo_name 
+      t('sufia.product_name')
+   end 
+
+   def get_page_title
+      t('sufia.controls.' + action_name)
+   end
+
+   def default_page_title
+      get_page_title + ' // ' + get_repo_name
    end
 
    def static_page_h1 
-       action_name.titleize
+      get_page_title
    end
+
+
 end 
 
